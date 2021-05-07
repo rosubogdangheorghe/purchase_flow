a442d2aee91c6071d66c0bcc482f7764b246f327
Ion <vaniniabivol@gmail.com>
2021-05-07 19:52:01 +0300

    code 07.05

--- src/main/java/com/roki/purchase/App.java
+++ src/main/java/com/roki/purchase/App.java
@@ -5,8 +5,7 @@ import com.roki.purchase.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 
 
-public class App
-{
+public class App {
     public static void main( String[] args )
     {
         SpringApplication.run(AppConfig.class,args);
